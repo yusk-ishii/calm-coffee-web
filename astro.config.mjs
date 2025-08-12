9b5bae61ebd515ebf1a290e65dcb9b0bfab6bf9a
Yusuke Ishii <y.retro404@gmail.com>
2025-08-12 20:38:57 +0900

    feat: レスポンシブ対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "./src/styles/functions.scss" as *;
+          @use "./src/styles/mixins.scss" as *;
+          `,
+        },
+      },
+    },
+  },
+});
