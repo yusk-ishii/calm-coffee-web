2fe672584fb83971cf77414db06f9ac1bc54f0a7
Yusuke Ishii <y.retro404@gmail.com>
2025-08-20 20:05:23 +0900

    feat: menu作成、全体のブラッシュアップ

---
+++ src/lib/menu.ts
@@ -0,0 +1,14 @@
+const html = document.documentElement;
+const menuButton = document.getElementById('js-menu-button');
+const nav = document.getElementById('js-nav');
+const navLinks = nav?.querySelectorAll('.nav-list a');
+
+menuButton?.addEventListener('click', () => {
+  html.classList.toggle("open")
+})
+
+navLinks?.forEach(link => {
+  link.addEventListener("click", () => {
+    html.classList.remove('open');
+  })
+})
