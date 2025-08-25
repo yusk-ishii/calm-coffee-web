2169df767845ba45a9d098540fe64fe652e52fae
Yusuke Ishii <y.retro404@gmail.com>
2025-08-25 18:06:50 +0900

    refactor: 画像の最適化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tokyo-designplex.jp",
-  base: "/~2504ishii/project",
+  site: 'https://tokyo-designplex.jp',
+  base: '/~2504ishii/project',
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -17,4 +19,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [compress()],
 });
